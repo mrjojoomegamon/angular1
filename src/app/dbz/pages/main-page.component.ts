2d688523470f03af5dbef41da5a910d83700c038
mr.jojo <mr.jojo.omegamon@outlook.com>
2024-07-02 22:23:38 -0600

    first commit

---
+++ src/app/dbz/pages/main-page.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { Character } from '../interfaces/character.interface';
+
+@Component({
+  selector: 'app-dbz-main-page',
+  templateUrl: './main-page.component.html',
+})
+
+export class MainPageComponent{
+
+  public characters: Character[]= [
+    {
+      name: 'Goku',
+      power: 10000
+    },
+    {
+      name: 'Vegeta',
+      power: 9000000000
+    },
+    {
+      name: 'Trunks',
+      power: 10000
+    }
+  ];
+
+
+}
